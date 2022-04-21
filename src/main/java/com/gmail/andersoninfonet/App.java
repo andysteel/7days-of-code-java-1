d67bdd95922633279e480e7c6588624e01ff5186
Anderson Dias <andersoninfonet@gmail.com>
2022-04-21 19:06:56 -0300

    feature genarate html lfile

--- src/main/java/com/gmail/andersoninfonet/App.java
+++ src/main/java/com/gmail/andersoninfonet/App.java
@@ -1,6 +1,10 @@
 package com.gmail.andersoninfonet;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 import com.gmail.andersoninfonet.services.ImbdGetTopMoviesService;
+import com.gmail.andersoninfonet.utils.HTMLGenerator;
 import com.gmail.andersoninfonet.utils.JsonParser;
 import com.gmail.andersoninfonet.utils.LoadProperties;
 
@@ -10,15 +14,22 @@ public class App {
         var baseUrl = LoadProperties.getValue("api.base.url");
         var apiKey = LoadProperties.getValue("api.key");
 
-        ImbdGetTopMoviesService
-            .getInstance()
-            .getTopMovies(baseUrl, apiKey)
-            .thenApply(JsonParser::extractJson)
-            .thenApply(JsonParser::extractJsonMovies)
-            .thenApply(JsonParser::preFormatMovieArrayToCreateMovie)
-            .thenApply(JsonParser::createMovies)
-            .thenAccept( movies -> movies.forEach(System.out::println))
-            .join();
+        var file = new File("index.html");
+        try (var printWriter = new PrintWriter(file);) {
+            var htmlGenerator = new HTMLGenerator(printWriter);
+
+            ImbdGetTopMoviesService
+                .getInstance()
+                .getTopMovies(baseUrl, apiKey)
+                .thenApply(JsonParser::extractJson)
+                .thenApply(JsonParser::extractJsonMovies)
+                .thenApply(JsonParser::preFormatMovieArrayToCreateMovie)
+                .thenApply(JsonParser::createMovies)
+                .thenAccept(htmlGenerator::generate)
+                .join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
     
